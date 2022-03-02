843cc278b6a9c7f2084b60d66231486955b88e7e
Ivan <30564568+ivan-camacho@users.noreply.github.com>
2022-03-02 18:28:52 -0300

    Create menu-base.c

---
+++ menu-base.c
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+    printf("**************MENU***************\n");
+    printf("*PRODUCTO       PRECIO          *\n");
+    printf("*Hamburguesa    $600            *\n");
+    printf("*Papas fritas   $300            *\n");
+    printf("*Gaseosa        $250            *\n");
+    printf("*Helado         $300            *\n");
+    printf("*Ensalada       $200            *\n");
+    printf("*********************************\n");
+    
+
+    return 0;
+}
