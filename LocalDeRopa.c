0254b5a408cf35d8a2a45e567b25c6750d56ed20
Ivan <30564568+ivan-camacho@users.noreply.github.com>
2022-03-09 17:04:25 -0300

    Create LocalDeRopa.c
    Clase 09/03/2022

---
+++ LocalDeRopa.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int cantRemeras,cantJeans,cantCamperas,cantCalzas,cantPolleras;
+
+    printf("**********LAS LOCAS************\n");
+    printf("*PRODUCTO        *PRECIO      *\n");
+    printf("*Remera          *$2500       *\n");
+    printf("*Jean            *$5000       *\n");
+    printf("*Campera         *$8000       *\n");
+    printf("*Calzas          *$3000       *\n");
+    printf("*Pollera         *$3500       *\n");
+    printf("*******************************\n");
+
+    printf("\nINGRESE LA CANTIDAD DE REMERAS QUE DESEA LLEVAR : ");
+    scanf("%d",&cantRemeras);
+
+    printf("\nINGRESE LA CANTIDAD DE JEANS QUE DESEA LLEVAR : ");
+    scanf("%d",&cantJeans);
+
+    printf("\nINGRESE LA CANTIDAD DE CAMPERAS QUE DESEA LLEVAR : ");
+    scanf("%d",&cantCamperas);
+
+    printf("\nINGRESE LA CANTIDAD DE CALZAS QUE DESEA LLEVAR : ");
+    scanf("%d",&cantCalzas);
+
+    printf("\nINGRESE LA CANTIDAD DE POLLERAS QUE DESEA LLEVAR : ");
+    scanf("%d",&cantPolleras);
+}
